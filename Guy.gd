ddcedda75bcc04426453d0b8db9f385e95827472
Andrey Gladyshev <vodomerkin@gmail.com>
2020-08-19 03:41:55 +0600

    Unstoppable Terminator type enemy.

--- Guy.gd
+++ Guy.gd
@@ -4,7 +4,8 @@ class_name Guy
 
 const TEAM := "Guy"
 const TEAM_PROJECTILE := TEAM + "Projectile"
-const SOCIAL_DISTANCE := 60.0
+const SOCIAL_DISTANCE := 30.0
+const NOT_INTERESTED := 800.0
 
 var rnd = RandomNumberGenerator.new()
 var target: Node2D
@@ -22,16 +23,34 @@ func _process(delta):
 	if target == null:
 		movement = Common.STAY
 	else:
-		if _targetVisible() and target.position.distance_to(position) > SOCIAL_DISTANCE:
+		if target.position.distance_to(position) > NOT_INTERESTED:
+			print_debug(name +  ": target lost.")
+			target = null
+			return
+
+		var targetVisible := _targetVisible()
+		if targetVisible and target.position.distance_to(position) > SOCIAL_DISTANCE:
 			if target.position.x > position.x:
 				movement = Common.RIGHT
 			if target.position.x < position.x:
 				movement = Common.LEFT
 		else:
 			movement = Common.STAY
-
-	if rnd.randf_range(0, 200) < 50:
-		shoot(TEAM_PROJECTILE)
+		var needToShoot := false
+		if targetVisible:
+			if target.position.x > position.x - 5 and target.position.x < position.x + 5 and target.position.y < position.y:
+				setAim(Common.UP)
+				needToShoot = true
+			elif target.position.x > position.x - 5 and target.position.x < position.x + 5 and target.position.y > position.y:
+				setAim(Common.DOWN)
+				needToShoot = true
+			else:
+				setAim(Common.STRAIGHT)
+				if target.position.y > position.y - 6 and target.position.y < position.y + 6:
+					needToShoot = true
+		
+		if needToShoot and rnd.randi_range(0, 1000) < 100:
+			shoot(TEAM_PROJECTILE)
 
 func _targetVisible() -> bool:
 	if target != null:
@@ -44,7 +63,3 @@ func _targetVisible() -> bool:
 func _on_DetectionArea_body_entered(body: Node2D):
 	if body.is_in_group(Player.TEAM):
 		target = body
-
-func _on_DetectionArea_body_exited(body):
-	if body.is_in_group(Player.TEAM):
-		target = null
